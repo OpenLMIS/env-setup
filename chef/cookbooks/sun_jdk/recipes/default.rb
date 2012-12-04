e4f8112bba0228f494cb2a28d8e1313180866285
Pratima Singh <pratima@thoughtworks.com>
2012-12-04 13:29:00 +0530

    [Pratima] Adding clause to notifies

--- chef/cookbooks/sun_jdk/recipes/default.rb
+++ chef/cookbooks/sun_jdk/recipes/default.rb
@@ -7,10 +7,16 @@
 # All rights reserved - Do Not Redistribute
 #
 
+execute "Remove OpenJDK" do
+  command "rpm -e java-1.6.0-openjdk-demo-1.6.0.0-1.50.1.11.5.el6_3.x86_64 java-1.6.0-openjdk-1.6.0.0-1.50.1.11.5.el6_3.x86_64 java-1.6.0-openjdk-javadoc-1.6.0.0-1.50.1.11.5.el6_3.x86_64 java-1.6.0-openjdk-src-1.6.0.0-1.50.1.11.5.el6_3.x86_64 java-1.6.0-openjdk-devel-1.6.0.0-1.50.1.11.5.el6_3.x86_64"
+  only_if "rpm -qa | grep openjdk"
+  action :run
+end
+
 cookbook_file "/tmp/jdk-7u9-linux-x64.rpm" do
   source "jdk-7u9-linux-x64.rpm"
   mode "0775"
-  notifies :run, "execute[Install Java]"
+  notifies :run, "execute[Install Java]", :immediately
 end
 
 execute "Install Java" do
