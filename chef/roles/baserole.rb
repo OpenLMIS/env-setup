9fa201aff0d5e0b39d21c234f447e2e5841fe427
Pratima Singh <pratima@thoughtworks.com>
2012-12-05 17:58:31 +0530

    [Pratima] Converting nagios_nrpe into role

--- chef/roles/baserole.rb
+++ chef/roles/baserole.rb
@@ -1,7 +1,3 @@
 name "baserole"
 description "Installs basic packages for OpenLMIS"
-default_attributes(
-                   "nagios" => {
-                     "server_ip" => "192.168.34.5"}
-                  )
-run_list  "recipe[git]", "recipe[selinux::non_persistent_disable]", "recipe[sun_jdk]", "recipe[gradle]", "recipe[postgres]", "recipe[tomcat]", "recipe[nagios_nrpe]"
+run_list  "recipe[git]", "recipe[selinux::non_persistent_disable]", "recipe[sun_jdk]", "recipe[gradle]", "recipe[postgres]", "recipe[tomcat]", "role[nagios_nrpe]"
