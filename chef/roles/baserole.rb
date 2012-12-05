530394c75bf5997f0a27992966bc3d4853f61bf0
Pratima Singh <pratima@thoughtworks.com>
2012-12-05 17:57:40 +0530

    [Pratima] Adding nagios_nrpe to base role

--- chef/roles/baserole.rb
+++ chef/roles/baserole.rb
@@ -1,3 +1,7 @@
 name "baserole"
 description "Installs basic packages for OpenLMIS"
-run_list  "recipe[git]", "recipe[selinux::non_persistent_disable]", "recipe[sun_jdk]", "recipe[gradle]", "recipe[postgres]", "recipe[tomcat]"
+default_attributes(
+                   "nagios" => {
+                     "server_ip" => "192.168.34.5"}
+                  )
+run_list  "recipe[git]", "recipe[selinux::non_persistent_disable]", "recipe[sun_jdk]", "recipe[gradle]", "recipe[postgres]", "recipe[tomcat]", "recipe[nagios_nrpe]"
