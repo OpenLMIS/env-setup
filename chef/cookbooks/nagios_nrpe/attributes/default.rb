736fcf24ec0b11a340b9a1ada92f85d2ddf6dccd
Pratima Singh <pratima@thoughtworks.com>
2012-12-05 18:01:20 +0530

    [Pratima] Deleting unused data

--- chef/cookbooks/nagios_nrpe/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-node["nagios"] ||= {}
-node["nrpe"] ||= {}
-node["nagios"]["plugins_dir"] = "/usr/lib64/nagios/plugins"
-node['hw_nagios_server'] = "10.10.1.96"
--- chef/cookbooks/nagios_nrpe/spec/default_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'chefspec'
-
-describe 'nagios_nrpe::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'nagios_nrpe::default' }
-  it 'should install nrpe_packages' do
-	chef_run.should install_package "nrpe"
-  end
-end
