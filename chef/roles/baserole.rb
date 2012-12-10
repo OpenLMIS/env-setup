b7568824f5067a876d8db4863991a2be33b8d426
Pratima Singh <pratima@thoughtworks.com>
2012-12-10 17:30:20 +0530

    [+Vinay] Removing selinux from baserole

--- chef/roles/baserole.rb
+++ chef/roles/baserole.rb
@@ -1,3 +1,3 @@
 name "baserole"
 description "Installs basic packages for OpenLMIS"
-run_list  "recipe[git]", "recipe[selinux::non_persistent_disable]", "recipe[sun_jdk]", "recipe[gradle]", "recipe[postgres]", "recipe[tomcat]", "role[nagios_nrpe]"
+run_list  "recipe[git]", "recipe[sun_jdk]", "recipe[gradle]", "recipe[postgres]", "recipe[tomcat]", "role[nagios_nrpe]"
