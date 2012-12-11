e250cfd42cfd730d9352c820495e2bfbe91bd078
Pratima Singh <pratima@thoughtworks.com>
2012-12-11 12:20:56 +0530

    [+Vinay] Setting up gradle home for openlmis user

--- chef/cookbooks/gradle/recipes/default.rb
+++ chef/cookbooks/gradle/recipes/default.rb
@@ -9,18 +9,18 @@
 
 script "Gradle Installation" do
   interpreter "bash"
-  user "root"
-  cwd "/usr"
+  user "openlmis"
+  cwd node[:webapp][:home]
   code <<-EOH
-  cd /usr
+  cd #{node[:webapp][:home]} 
   wget http://services.gradle.org/distributions/gradle-1.3-all.zip
-  unzip -o /usr/gradle-1.3-all.zip
+  unzip -o #{node[:webapp][:home]}/gradle-1.3-all.zip
   EOH
   not_if "gradle -v | grep Gradle"
   notifies :run, "execute[Set gradle home]", :immediately
 end
 
 execute "Set gradle home" do
-  command 'echo "export PATH=/usr/gradle-1.3/bin:$PATH" >> ~/.bashrc'
+  command "export PATH=#{node[:webapp][:home]}/gradle-1.3/bin:$PATH"
   action :nothing
 end
