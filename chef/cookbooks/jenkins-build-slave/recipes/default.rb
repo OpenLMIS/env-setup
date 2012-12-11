1ee4954a778a4f991af4f5c83e28e9b9b494a2c7
Pratima Singh <pratima@thoughtworks.com>
2012-12-11 15:20:14 +0530

    [+Vinay] Adding jenkins slave cookbook

---
+++ chef/cookbooks/jenkins-build-slave/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: jenkins-build-slave
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+cookbook_file "/etc/yum.repos.d/jenkins.repo" do
+  source "jenkins.repo"
+  owner "root"
+  group "root"
+  mode  "0644"
+end
+
+package "jenkins"
+
+service "jenkins" do
+  action :start
+end
