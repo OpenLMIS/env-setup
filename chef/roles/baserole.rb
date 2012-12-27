180fec1907c6bd832397efec296858ac7dac2ee3
Pratima Singh <pratima@thoughtworks.com>
2012-12-27 11:39:27 +0530

    Altering sequence of recipes

--- chef/roles/baserole.rb
+++ chef/roles/baserole.rb
@@ -1,3 +1,3 @@
 name "baserole"
 description "Installs basic packages for OpenLMIS"
-run_list  "recipe[iptables]","recipe[git]", "recipe[java]", "recipe[gradle]", "recipe[postgres]", "recipe[tomcat]", "role[nagios_nrpe]", "recipe[openlmis_config]", "recipe[jenkins-build-slave]"
+run_list  "recipe[iptables]","recipe[openlmis_config]","recipe[git]", "recipe[java]", "recipe[gradle]", "recipe[postgres]", "recipe[tomcat]", "role[nagios_nrpe]"
