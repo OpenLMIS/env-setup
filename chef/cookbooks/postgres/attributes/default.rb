acfc9c2d798928483ead13c4519419d2a526ae3f
Pratima Singh <pratima@thoughtworks.com>
2012-12-27 11:49:17 +0530

    Converting attribute format for allowed host subnets

--- chef/cookbooks/postgres/attributes/default.rb
+++ chef/cookbooks/postgres/attributes/default.rb
@@ -1,3 +1,2 @@
 node["postgreSQL"]={}
-node["postgreSQL"]["hostSubnet"]="192.168.34.0/24"
-node["postgreSQL"]["devSubnet"]="10.15.6.0/24"
+node["postgreSQL"]["allowSubnet"]=["192.168.34.0/24", "10.15.6.0/24"]
