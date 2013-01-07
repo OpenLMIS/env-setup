49718461ee64209dbbbddd323517944d68589805
root <root@OpenLmis.(none)>
2013-01-07 15:19:33 +0530

    [pratima/vinay] added cookbook for logrotate

---
+++ chef/cookbooks/logrotate/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: logrotate
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+#package "logrotate" do
+#	action :install
+#end
+
+cron "logrotate" do
+	command "/usr/sbin/logrotate -f /etc/logrotate.d/openlmis"
+	hour "1"
+end
+
+template "/etc/logrotate.d/openlmis" do
+	source "openlmis.erb"
+	mode "0644"
+	owner "root"
+	group "root"
+end
