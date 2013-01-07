49413c117fe3afffe6840f1c2a64cdfc1b92963d
Pratima Singh <pratima@thoughtworks.com>
2013-01-07 12:25:48 +0530

    Updated metadata with EPL license for all cookbooks

--- chef/cookbooks/java/metadata.rb
+++ chef/cookbooks/java/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "ThoughtWorks - DevOps Team | Pratima Singh, Vinay Mor"
 maintainer_email "pratima@thoughtworks.com, vinaymor@thoughtworks.com"
-license          "ThoughtWorks OpenSource license"
+license          "Eclipse Public License"
 description      "Installs/Configures java"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
