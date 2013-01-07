0de099f0dd71e604c50ef1197c9996ecf9abe6d4
Pratima Singh <pratima@thoughtworks.com>
2013-01-07 15:45:45 +0530

    Cookbook refactoring

--- chef/cookbooks/gradle/recipes/default.rb
+++ chef/cookbooks/gradle/recipes/default.rb
@@ -5,16 +5,23 @@
 #
 # All rights reserved - Do Not Redistribute
 
+
+remote_file "#{node["webapp"]["home"]}/gradle-1.3-all.zip" do
+  source "http://services.gradle.org/distributions/gradle-1.3-all.zip"
+  owner "openlmis"
+  root "openlmis"
+  mode "0755"
+end
+
 script "Gradle Installation" do
   interpreter "bash"
   user "openlmis"
   cwd node["webapp"]["home"]
   code <<-EOH
   cd #{node["webapp"]["home"]} 
-  wget http://services.gradle.org/distributions/gradle-1.3-all.zip
   unzip -o #{node["webapp"]["home"]}/gradle-1.3-all.zip
   EOH
-  not_if "gradle -v | grep Gradle"
+  not_if "test -f #{node["webapp"]["home"]}/gradle-1.3-all.zip"
   notifies :run, "execute[Set gradle home]", :immediately
 end
 
