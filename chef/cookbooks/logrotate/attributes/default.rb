4a82a0cfef9a77649305b39ed8709acee4b6cd48
vinaymor002 <vinaymor002@gmail.com>
2013-01-08 10:49:06 +0530

    refactored logrotate cookbook

--- chef/cookbooks/logrotate/attributes/default.rb
+++ chef/cookbooks/logrotate/attributes/default.rb
@@ -1,1 +1,2 @@
 node["tomcat_package"]="apache-tomcat-7.0.33"
+node["webapp_home"]="/home/openlmis"
--- chef/cookbooks/logrotate/recipes/default.rb
+++ chef/cookbooks/logrotate/recipes/default.rb
@@ -6,13 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-#package "logrotate" do
-#	action :install
-#end
+package "logrotate" do
+	action :install
+end
 
 cron "logrotate" do
-	command "/usr/sbin/logrotate -f /etc/logrotate.d/openlmis"
-	hour "1"
+	command "/usr/sbin/logrotate -v /etc/logrotate.d/openlmis"
+	day "1"
 end
 
 template "/etc/logrotate.d/openlmis" do
