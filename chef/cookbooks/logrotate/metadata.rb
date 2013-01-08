2df985bdcd4c1d53e298a7affce02112f8a2bca6
Pratima Singh <pratima@thoughtworks.com>
2013-01-08 11:54:35 +0530

    Modifying metadata

--- chef/cookbooks/logrotate/metadata.rb
+++ chef/cookbooks/logrotate/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures logrotate"
+maintainer       "ThoughtWorks - DevOps Team | Pratima Singh, Vinay Mor"
+maintainer_email "pratima@thoughtworks.com, vinaymor@thoughtworks.com"
+license          "Eclipse Public License"
+description      "Configures logrotate"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "1.0.0"
