c40f030a104d67424a705674aa016706db19aa44
vinaymor002 <vinaymor002@gmail.com>
2013-01-08 18:59:13 +0530

    spec tests for java, httpd

--- chef/cookbooks/httpd/recipes/default.rb
+++ chef/cookbooks/httpd/recipes/default.rb
@@ -1,20 +1,4 @@
-# Cookbook Name:: httpd
-# Recipe:: default
-#
-# Copyright 2011
-#
-# All rights reserved - Do Not Redistribute
-
-package "httpd" do
-  package_name "httpd"
-  action :install
+case node["platform"]
+when "centos"
+	include_recipe "httpd::centos"
 end
-
-service "httpd" do
-  service_name "httpd"
-  restart_command "/sbin/service httpd restart && sleep 1"
-  reload_command "/sbin/service httpd reload && sleep 1"    
-
-  action :enable 
-end
-
