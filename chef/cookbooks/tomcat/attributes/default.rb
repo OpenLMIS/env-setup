726c7a8f3bbb1d294b36d5747a1990906e1ae56a
Pratima Singh <pratima@thoughtworks.com>
2013-01-11 11:26:31 +0530

    Modifying attribute

--- chef/cookbooks/tomcat/attributes/default.rb
+++ chef/cookbooks/tomcat/attributes/default.rb
@@ -4,4 +4,4 @@ node.set["webapp"]={}
 node.set["webapp"]["home"]="/home/openlmis"
 node.set["webapp"]["user"]="openlmis"
 node.set["java"]["home"]="/usr/java/jdk1.7.0_09"
-node.set["tomcat"]["home"]="#{node["webapp"]["home"]}/apache-tomcat-7.0.33/bin"
+node.set["tomcat"]["home"]="#{node["webapp"]["home"]}/apache-tomcat-7.0.33"
