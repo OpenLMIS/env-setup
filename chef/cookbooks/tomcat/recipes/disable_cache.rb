63e9e040d27c3e85c7be1c48f5dfebce742fdaf0
Pratima Singh <pratima@thoughtworks.com>
2013-01-11 16:15:01 +0530

    [+Vinay] Fixing syntax error

--- chef/cookbooks/tomcat/recipes/disable_cache.rb
+++ chef/cookbooks/tomcat/recipes/disable_cache.rb
@@ -6,7 +6,7 @@
 # All rights reserved - Do Not Redistribute
 
 execute "Disable Cache" do
-  command " sed -i 's/<Context>/<Context cachingAllowed='false'>/g' #{node["tomcat"]["home"]}/conf/context.xml"
+  command " sed -i 's/<Context>/<Context cachingAllowed=\"false\">/g' #{node["tomcat"]["home"]}/conf/context.xml"
   action :run
   notifies :restart, "service[tomcat]", :immediately
   not_if "grep 'Context cachingAllowed='false'' #{node["tomcat"]["home"]}/conf/context.xml"
