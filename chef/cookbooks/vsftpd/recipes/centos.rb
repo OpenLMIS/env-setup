fda3a635d9f9062c8c5a66c85fca222ba52199ed
Manan <manan.bharara@gmail.com>
2013-04-16 16:05:15 +0530

    |#678|+Vinay| Configuring vsftpd cookbook.

--- chef/cookbooks/vsftpd/recipes/centos.rb
+++ chef/cookbooks/vsftpd/recipes/centos.rb
@@ -1,14 +1,13 @@
-yum_package "vsftpd" do
-	action :install
-end
-yum_package "db4-utils db4" do
-	action :install
+%w{vsftpd db4 db4-utils}.each do |package|
+	yum_package "#{package}" do
+		action :install
+	end
 end
 
-cookbook_file "/etc/vsftpd/vsftpd.conf" do
+cookbook_file "/etc/vsftpd/vsftpd.conf" do	
 	source "vsftpd.conf"
 	action :create
-	mode 0755
+	mode 0600
 	owner "root"
 	group "root"
 end
@@ -16,7 +15,7 @@ end
 cookbook_file "/etc/vsftpd/vusers.txt" do
 	source "virtual_users.txt"
 	action :create
-	mode 0755
+	mode 0600
 	owner "root"
 	group "root"
 end
@@ -29,7 +28,7 @@ end
 cookbook_file "/etc/pam.d/vsftpd.virtual" do
 	source "vsftpd.virtual"
 	action :create
-	mode 0755
+	mode 600
 	owner "root"
 	group "root"
 end
@@ -37,23 +36,39 @@ end
 directory "/home/vftp" do
 	action :create
 	owner "root"
-	owner "root"
+	group "root"
 end
 
-%w{'openlmis' 'erp'}.each do |dir|
-	directory "/home/vftp/#{dir}" do
+directory "/home/vftp/openlmis" do
 		mode 0755
 		action :create
 		owner "root"
-		create "root"
-	end
+	 	group "root"
 end
 
 execute "change mode for directory" do
 	command "chown -R ftp:ftp /home/vftp"
 end
 
+directory "/etc/vsftpd/user_conf" do
+	action :create
+	owner "root"
+	group "root"
+end
+
+%w{openlmis erp}.each do |file_name|
+	cookbook_file "/etc/vsftpd/user_conf/#{file_name}" do
+		action :create
+		source "#{file_name}"
+		mode 600
+		owner "root"
+		group "root"
+		
+	end
+end
+	
 service "vsftpd" do
 	action [:enable ,:start]
 end
+
 
