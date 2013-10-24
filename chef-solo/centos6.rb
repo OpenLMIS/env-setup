95ed089e0e308fdec93f90ae6979f204b7380819
vinaymor002 <vinaymor002@gmail.com>
2013-10-25 02:22:44 +0530

    |000| +Rahul| running bootstrap commands with sudo , and changing some scripts to run as sudo

--- chef-solo/centos6.rb
+++ chef-solo/centos6.rb
@@ -3,44 +3,41 @@ bash -c
 
 # ADD EPEL & RBEL REPOS
 # ======================
-rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm
+sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm
 
-rpm -Uvh http://rbel.co/rbel6
+sudo rpm -Uvh http://rbel.co/rbel6
 # END REPOS
 # ======================
 
 
 if [ ! -f /usr/bin/chef-client ]; then
 
-  yum -y install gcc gcc-c++ kernel-devel make wget libxslt-devel libxml2-devel git autoconf flex bison openssl-devel libcurl-devel readline-devel kernel-headers zlib-devel ruby ruby-devel ruby-libs ruby-irb ruby-rdoc ruby-ri unzip rubygems ntp
+sudo yum -y install gcc gcc-c++ kernel-devel make wget libxslt-devel libxml2-devel git autoconf flex bison openssl-devel libcurl-devel readline-devel kernel-headers zlib-devel ruby ruby-devel ruby-libs ruby-irb ruby-rdoc ruby-ri unzip rubygems ntp
   
 fi
 
 #### CLOCK SYNC #####
-ntpdate pool.ntp.org
-chkconfig ntpd on
-service ntpd start
+sudo ntpdate pool.ntp.org
+sudo chkconfig ntpd on
+sudo service ntpd start
 
 #### Installing chef ####
-gem update --system
-gem install ohai --no-rdoc --no-ri --verbose
-gem install chef --no-rdoc --no-ri --verbose <%= bootstrap_version_string %>
+sudo gem update --system
+sudo gem install ohai --no-rdoc --no-ri --verbose
+sudo gem install chef --no-rdoc --no-ri --verbose <%= bootstrap_version_string %>
 
 #### Create OpenLMIS User ####
-useradd openlmis -c "For eLMIS Application"
+sudo useradd openlmis -c "For eLMIS Application"
 
 #### Remove tty from sudoers defaults ####
-echo "openlmis        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
-sed -i "s/Defaults    requiretty/#Defaults    requiretty/g" /etc/sudoers
+sudo bash -c "echo \"openlmis        ALL=(ALL)       NOPASSWD: ALL\" >> /etc/sudoers"
+sudo sed -i "s/Defaults    requiretty/#Defaults    requiretty/g" /etc/sudoers
 
 #### Chef Setup ####
-mkdir -p /etc/chef
+sudo mkdir -p /etc/chef
 
-(
-cat <<EOP
-recipe_url "https://github.com/OpenLMIS/env-setup/blob/master/chef-solo/chef-solo.tar.gz?raw=true" 
-json_attribs "https://raw.github.com/OpenLMIS/env-setup/master/chef-solo/solo.json" 
-EOP
-) > /etc/chef/solo.rb
+
+sudo bash -c $'echo \'recipe_url \"https://github.com/OpenLMIS/env-setup/blob/master/chef-solo/chef-solo.tar.gz?raw=truez\"\' > /etc/chef/solo.rb'
+sudo bash -c $"echo \'json_attribs \"https://raw.github.com/OpenLMIS/env-setup/master/chef-solo/solo.json\"\' >> /etc/chef/solo.rb'
 
 chef-solo -c /etc/chef/solo.rb
